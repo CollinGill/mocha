b64efa83cdad0821bae5c2e4c4c29464abc36d06
CollinGill <collingill02@gmail.com>
2023-11-19 14:17:22 -0500

    Arg error

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let usage= "mocha path/to/file.moc";
 
     if args.len() < 2 {
-        eprintln!("Error: Not enough arguments");
+        eprintln!("Error: Incorrect usage, correct usage is...");
+        println!("{}", usage);
     }
+
+    let src_code = &args[1];
 }
