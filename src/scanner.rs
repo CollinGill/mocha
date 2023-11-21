3a1bf27d126c8b2c962466c5cb27c74a8d7d92d2
CollinGill <collingill02@gmail.com>
2023-11-21 16:01:54 -0500

    Tokenization working, need to complete grammar then implement it into the scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -14,18 +14,25 @@ impl Scanner {
 
         loop {
             match self.peek(0) {
+                None => {break;}
+
                 Some(cur_) => {
-                    if cur_.is_whitespace() {
-                        if cur_ == '\n' {
+                    if cur_.is_whitespace()
+                    {
+                        if cur_ == '\n'
+                        {
                             self.line += 1;
                             self.col = 0;
-                        } else {
-                            self.col += 1;
                         }
 
-                        self.consume();
+                        else { self.col += 1; }
 
-                    } else if cur_ == ';' {
+                        self.consume(1);
+
+                    }
+
+                    else if cur_ == ';'
+                    {
                         let tok = Token {
                             line: self.line,
                             col: self.col,
@@ -35,9 +42,12 @@ impl Scanner {
 
                         self.tokens.push(tok);
                         self.col += 1;
-                        self.consume();
+                        self.consume(1);
 
-                    } else if cur_ == ',' {
+                    }
+
+                    else if cur_ == ','
+                    {
                         let tok = Token {
                             line: self.line,
                             col: self.col,
@@ -47,9 +57,12 @@ impl Scanner {
 
                         self.tokens.push(tok);
                         self.col += 1;
-                        self.consume();
+                        self.consume(1);
 
-                    } else if cur_ == '(' {
+                    }
+
+                    else if cur_ == '('
+                    {
                         let tok = Token {
                             line: self.line,
                             col: self.col,
@@ -59,9 +72,12 @@ impl Scanner {
 
                         self.tokens.push(tok);
                         self.col += 1;
-                        self.consume();
+                        self.consume(1);
 
-                    } else if cur_ == ')' {
+                    }
+
+                    else if cur_ == ')'
+                    {
                         let tok = Token {
                             line: self.line,
                             col: self.col,
@@ -71,9 +87,12 @@ impl Scanner {
 
                         self.tokens.push(tok);
                         self.col += 1;
-                        self.consume();
+                        self.consume(1);
 
-                    } else if cur_ == '{' {
+                    }
+
+                    else if cur_ == '{'
+                    {
                         let tok = Token {
                             line: self.line,
                             col: self.col,
@@ -83,9 +102,12 @@ impl Scanner {
 
                         self.tokens.push(tok);
                         self.col += 1;
-                        self.consume();
+                        self.consume(1);
 
-                    } else if cur_ == '}' {
+                    }
+
+                    else if cur_ == '}'
+                    {
                         let tok = Token {
                             line: self.line,
                             col: self.col,
@@ -95,9 +117,12 @@ impl Scanner {
 
                         self.tokens.push(tok);
                         self.col += 1;
-                        self.consume();
+                        self.consume(1);
 
-                    } else if cur_ == ':' {
+                    }
+
+                    else if cur_ == ':'
+                    {
                         let tok = Token {
                             line: self.line,
                             col: self.col,
@@ -107,17 +132,68 @@ impl Scanner {
 
                         self.tokens.push(tok);
                         self.col += 1;
-                        self.consume();
+                        self.consume(1);
 
-                    } else if cur_.is_alphabetic() || cur_ == '_' {
+                    }
+
+                    else if cur_ == '='
+                    {
+                        let tok:  Token;
+                        match self.peek(1) {
+                            Some(ch) => {
+                                if ch == '>' {
+                                    tok = Token {
+                                        line: self.line,
+                                        col: self.col,
+                                        type_: String::from("ARROW"),
+                                        value: String::from("=>")
+                                    };
+                                    self.col += 2;
+                                    self.consume(2);
+
+                                } else {
+                                    tok = Token {
+                                        line: self.line,
+                                        col: self.col,
+                                        type_: String::from("EQU"),
+                                        value: String::from("=")
+                                    };
+                                    self.col += 1;
+                                    self.consume(1);
+                                }
+                            }
+
+                            None => {
+                                tok = Token {
+                                    line: self.line,
+                                    col: self.col,
+                                    type_: String::from("EQU"),
+                                    value: String::from("=")
+                                };
+
+                                self.col += 1;
+                                self.consume(1);
+                            }
+                        }
+
+                        self.tokens.push(tok);
+
+                    }
+
+                    // Identifiers and keywords
+                    else if cur_.is_alphabetic() || cur_ == '_'
+                    {
                         loop {
                             match self.peek(0) {
                                 Some(cur) => {
-                                    if cur.is_alphanumeric() || cur == '_' {
+                                    if cur.is_alphanumeric() || cur == '_'
+                                    {
                                         buf.push(cur);
-                                        self.consume();
+                                        self.consume(1);
 
-                                    } else { break; }
+                                    }
+
+                                    else { break; }
                                 }
 
                                 None => { break; }
@@ -125,11 +201,11 @@ impl Scanner {
                         }
 
                         let token_name: String = buf.iter().collect();
-                        self.col += token_name.len();
-
+                        let token_len: usize = token_name.len();
                         let tok: Token;
 
-                        if token_name == "func" {
+                        if token_name == "func"
+                        {
                             tok = Token {
                                 line: self.line,
                                 col: self.col,
@@ -138,48 +214,182 @@ impl Scanner {
                             };
 
                             self.tokens.push(tok);
-                        } else if token_name == "return" {
+                        }
+
+                        else if token_name == "return"
+                        {
                             tok = Token {
                                 line: self.line,
                                 col: self.col,
                                 type_: String::from("RETURN"),
                                 value: String::from(token_name)
                             };
-
                             self.tokens.push(tok);
 
-                        } else if token_name == "void" {
+                        }
+
+                        else if token_name == "var"
+                        {
+                            tok = Token {
+                                line: self.line,
+                                col: self.col,
+                                type_: String::from("VAR"),
+                                value: String::from(token_name)
+                            };
+                            self.tokens.push(tok);
+                        }
+
+                        else if token_name == "const"
+                        {
+                            tok = Token {
+                                line: self.line,
+                                col: self.col,
+                                type_: String::from("CONST"),
+                                value: String::from(token_name)
+                            };
+                            self.tokens.push(tok);
+                        }
+
+                        // Data types
+                        else if token_name == "void"
+                            || token_name == "int"
+                            || token_name == "float"
+                            || token_name == "string"
+                            || token_name == "char"
+                        {
                             tok = Token {
                                 line: self.line,
                                 col: self.col,
                                 type_: String::from("TYPE"),
                                 value: String::from(token_name)
                             };
-
                             self.tokens.push(tok);
 
-                        } else {
+                        }
+
+                        else
+                        {
                             tok = Token {
                                 line: self.line,
                                 col: self.col,
                                 type_: String::from("IDENTIFIER"),
                                 value: String::from(token_name)
                             };
-
                             self.tokens.push(tok);
                         }
 
-                    } else if cur_.is_numeric() {
-                        // Placeholder
-                        self.consume();
+                        self.col += token_len;
+                        buf.clear();
 
-                    } else {
+                    }
+
+                    // Numbers
+                    else if cur_.is_numeric()
+                    {
+                        let mut has_radix = false;
+
+                        loop {
+                            match self.peek(0) {
+                                Some(cur) => {
+                                    if cur.is_numeric()
+                                    {
+                                        buf.push(cur);
+                                        self.consume(1);
+                                    }
+
+                                    else if cur == '.'
+                                    {
+                                        if has_radix
+                                        {
+                                            eprintln!("ERROR: Invalid syntax\nLine: {}\nCol: {}",
+                                                      self.line, self.col);
+                                        }
+
+                                        has_radix = true;
+                                        buf.push(cur);
+                                        self.consume(1);
+                                    }
+
+                                    else {break;}
+                                }
+
+                                None => {break;}
+                            }
+
+                            let token_value: String = buf.iter().collect();
+                            let token_len: usize = token_value.len();
+                            let tok: Token;
+
+                            if has_radix
+                            {
+                                tok = Token {
+                                    line:  self.line,
+                                    col: self.col,
+                                    type_: String::from("FLOAT-LIT"),
+                                    value: token_value
+                                };
+                            }
+
+                            else
+                            {
+                                tok = Token {
+                                    line:  self.line,
+                                    col: self.col,
+                                    type_: String::from("INT-LIT"),
+                                    value: token_value
+                                };
+                            }
+
+                            self.tokens.push(tok);
+                            self.col += token_len;
+                            buf.clear();
+                        }
+
+                    }
+
+                    // String literals
+                    else if cur_ == '"'
+                    {
+                        buf.push(cur_);
+                        self.consume(1);
+
+                        loop {
+                            match self.peek(0) {
+                                Some(cur) => {
+                                    buf.push(cur);
+                                    self.consume(1);
+                                    if cur == '"'
+                                    {
+                                        break;
+                                    }
+                                }
+
+                                None => {break;}
+                            }
+                        }
+
+                        let token_value: String = buf.iter().collect();
+                        let token_len: usize = token_value.len();
+
+                        let tok = Token {
+                            line: self.line,
+                            col: self.col,
+                            type_: String::from("STRING-LIT"),
+                            value: String::from(token_value)
+                        };
+
+                        self.tokens.push(tok);
+                        self.col += token_len;
+                        buf.clear();
+                    }
+
+                    else
+                    {
                         // Placeholder
-                        self.consume();
+                        self.col += 1;
+                        self.consume(1);
                     }
                 }
-
-                None => {break;}
             }
         }
     }
@@ -192,8 +402,8 @@ impl Scanner {
         }
     }
 
-    fn consume(&mut self) {
-        self.index += 1;
+    fn consume(&mut self, to_consume: usize) {
+        self.index += to_consume;
     }
 
 }
