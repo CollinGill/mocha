5ff47196ae89cac1e99b1a2c31fd9b24bfc0a71e
Collin Gillespie <collingill02@gmail.com>
2024-08-07 21:23:10 -0400

    arg parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, world!"
-              << std::endl;
+    if (argc != 2) {
+        std::cerr << "ERROR: Not enough aruments\n";
+        return 1;
+    }
+
+    std::string file_name = std::string(argv[1]);
+    if (!file_name.ends_with(".mocha")) {
+        std::cerr << "ERROR: " << file_name << " is not a valid Mocha file\n";
+        return 1;
+    }
 
     return 0;
 }
